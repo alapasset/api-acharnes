1b67592f599f64a401785e1670fc6a93f23ee6e3
Alexis Lapasset <alexis.lapasset@gmail.com>
2020-08-27 15:59:05 +0200

    fix postgres connection

--- src/connection/ConnectionToPostgres.ts
+++ src/connection/ConnectionToPostgres.ts
@@ -1,12 +1,10 @@
 import { createConnection } from "typeorm";
 
+const connectionString = process.env.DATABASE_URL || "postgres://pmbiqnsbeynozg:a777f0d776a49e5f6b63f1e3658f9205fa90c31e1e6eba6d54927b7d9a566fc7@ec2-54-217-206-236.eu-west-1.compute.amazonaws.com:5432/d2o6qso33kqu66"
+
 export const connectionToPostgres = createConnection({
     type: "postgres",
-    host: "ec2-54-217-206-236.eu-west-1.compute.amazonaws.com",
-    port:  5432,
-    username: "pmbiqnsbeynozg",
-    password: "a777f0d776a49e5f6b63f1e3658f9205fa90c31e1e6eba6d54927b7d9a566fc7",
-    database: "d2o6qso33kqu66",
+    url: connectionString,
     ssl: { rejectUnauthorized : false },
     entities: ["src/entity/**/*.ts"],
     migrations: ["src/migration/**/*.ts"],
